6e69dcf71731eecc4960cbb9ae94b784ace06afd
grigorkeropyan <goqorkeropyan@gmail.com>
2022-02-20 12:46:42 +0100

    added fucntion to run the rank reg algorithm for different datasets and betas and sabe boxplot

--- src/rank_regression.R
+++ src/rank_regression.R
@@ -40,6 +40,9 @@ inverse <- function(f, lower, upper){
   }
 }
 
+
+inverse_cdf_z<- inverse(cdf_z, -1000, 1000)
+
 #' Title finds beta coefficients using a fixed point iteration for a 
 #' whole data. Should be called only for small sizes of n(sample size) < 1000.
 #'
@@ -105,14 +108,15 @@ find_f1_coefs_fixed_point <- function(Y, X, tol=1e-9, max_iter=300) {
 #' @export
 #'
 #' @examples
-find_f1_coefs_fixed_point_stochastic <- function(Y, X, batch_size=64, tol=1e-9, max_iter=300) {
+find_f1_coefs_fixed_point_stochastic <- function(Y, X, batch_size=64, tol=1e-9, 
+                                                 max_iter=300) {
   if (!is.matrix(X)) {
     X <- as.matrix(X)
   }
   
   m <- ncol(X)
   n <- nrow(X)
-  coefs <- runif(m, min=-10, max=10)
+  coefs <- matrix(runif(m, min=-10, max=10), m, 1)
   # coefs <- beta
   
   err <- 1
@@ -122,12 +126,15 @@ find_f1_coefs_fixed_point_stochastic <- function(Y, X, batch_size=64, tol=1e-9, 
     batch_Y <- Y[random_inds, ]
     
     A <- t(batch_X) %*% batch_X
-    P <- inv(A) %*% t(batch_X)
+    if(nrow(A) > 1) {
+      P <- inv(A) %*% t(batch_X)
+    } else {
+      P <- 1/A[1, 1] * t(batch_X)
+    }
     ranks_Y <- rank(batch_Y)
     empirical_cdf_Y <- ranks_Y/(batch_size+1)
     
-    print(paste("iter  ", iter))
-    
+    # print(paste("iter  ", iter))
     sub_vals <- batch_X %*% coefs
     yhat <- as.vector(sapply(empirical_cdf_Y, inverse_cdf_z, arg=sub_vals))
     
@@ -197,89 +204,169 @@ simulate_rank_regression_data <- function(n, m) {
   return(res)
 }
 
+#' Title run rank regression algorithms 
+#' for specific arguments and number of datasets
+#'
+#' @param n number of samples in each dataset
+#' @param m number of coefficients
+#' @param number_of_datasets number of datasets
+#'
+#' @return ground truth betas and estimated betas
+#' @export
+#'
+#' @examples
+run_rank_regression_algorithms <- function(n=1000, m=1, number_of_datasets=100){
+  exponent <- function(a, pow) (abs(a)^pow)*sign(a)
+  betas <- c(0.01, 0.1, 1, 10, 50, 100, 300)
+  
+  rank_reg_est_betas <- matrix(0, nrow=number_of_datasets, ncol=length(betas))
+  for(i in 1:number_of_datasets) {
+    print(paste("dataset ", i))
+    X <- matrix(rnorm(n*m), n, m)
+    for (j in 1:length(betas)) {
+      print(paste("beta ", j))
+      beta <- betas[j]
+      noise <- rnorm(n)
+      Y <- X %*% beta + noise
+      Y <- exponent(Y, 1/3) + 4.7
+      
+      ranks_Y <- rank(Y)
+      ecdf_Y <- ranks_Y/(n+1)
+      
+      pred_beta <- find_f1_coefs_fixed_point_stochastic(Y, X, batch_size = 64, 
+                                                        max_iter = 100, tol=1e-9)
+      
+      rank_reg_est_betas[i, j] <- pred_beta
+    }
+  }
+  return (list("rank_reg_est_betas"=rank_reg_est_betas, "betas"=betas))
+}
+
+
+#' Title run rank regression algorithms and 
+#' save a boxplots of the estimated betas
+#'
+#' @param file_n file name where the plots should be saved
+#'
+#' @return ground truth betas and estimated betas
+#' @export
+#'
+#' @examples
+run_algorithms_and_save_plots <- function(file_n='/Users/grigorkeropyan/pnl_gaussian/plots/'){
+  res <- run_rank_regression_algorithms()
+  rank_reg_est_betas <- res$rank_reg_est_betas
+  betas <- res$betas
+  
+  dat <- stack(as.data.frame(rank_reg_est_betas))
+  
+  pl <- ggplot() + geom_boxplot(aes(x=dat$ind, y=dat$values, colour='estimated betas')) + 
+    geom_point(aes(x=unique(dat$ind), y=betas, colour='ground truth betas')) + 
+    labs(title="rank regression", x="",y="betas") +
+    scale_color_manual(name='legend', 
+                       breaks=c('estimated betas', 'ground truth betas'),
+                       values=c('black', 'red')) +
+    guides(colour = guide_legend(override.aes = list(
+      linetype = c("solid", "blank"),
+      color = c("black","red")
+    ))) +
+    # theme(legend.position=c(0.15,0.91), plot.title = element_text(hjust = 0.5))
+    theme(legend.position='top', plot.title = element_text(hjust = 0.5))
+  
+  # file_name <- paste(file_n, 'rank_reg_alg/', sep='')
+  file_name <- paste(file_n, 'rank_reg_box_plots.png', sep='')
+  ggsave(filename = file_name, plot = pl)
+  
+  plot(pl)
+  
+  return (res)
+}
+
+res <- run_algorithms_and_save_plots()
+res
+
 # data <- simulate_rank_regression_data(10000, 6)
 # saveRDS(data, '/Users/grigorkeropyan/pnl_gaussian/datasets/data1')
 # data <- readRDS('/Users/grigorkeropyan/pnl_gaussian/datasets/data1')
 # data
 
 # TODO maybe standardize the data
-run_fixed_point <- function(n, batch_size=64, max_iter=100, tol = 1e-5) {
-  data <- simulate_rank_regression_data(n, 6)
-  
-  X <- data$X
-  Y <- data$Y
-  beta <- data$beta
-  
-  ranks_Y <- rank(Y)
-  ecdf_Y <- ranks_Y/(n+1)
-  G_beta <- Gbeta(beta, ecdf_Y, X)
-  
-  pred_beta <- find_f1_coefs_fixed_point_stochastic(Y, X, batch_size = batch_size, 
-                                                    max_iter = max_iter, tol=tol)
-  
-  G_pred_beta <- Gbeta(pred_beta, ecdf_Y, X)
-  
-  res <- list("gt_beta"=beta, "pred_beta"=c(pred_beta), "G_beta"=c(G_beta), 
-              "G_pred_beta"=c(G_pred_beta),
-              "n"=nrow(X), "m"=ncol(X), "max_iter"=max_iter, 
-              "batch_size"=batch_size, "tol"=tol,
-              "l2_dist_betas"=sum((pred_beta - beta)**2)**0.5)
-  return (res)
-}
+# run_fixed_point <- function(n, batch_size=64, max_iter=100, tol = 1e-5) {
+#   data <- simulate_rank_regression_data(n, 6)
+#   
+#   X <- data$X
+#   Y <- data$Y
+#   beta <- data$beta
+#   
+#   ranks_Y <- rank(Y)
+#   ecdf_Y <- ranks_Y/(n+1)
+#   G_beta <- Gbeta(beta, ecdf_Y, X)
+#   
+#   pred_beta <- find_f1_coefs_fixed_point_stochastic(Y, X, batch_size = batch_size, 
+#                                                     max_iter = max_iter, tol=tol)
+#   
+#   G_pred_beta <- Gbeta(pred_beta, ecdf_Y, X)
+#   
+#   res <- list("gt_beta"=beta, "pred_beta"=c(pred_beta), "G_beta"=c(G_beta), 
+#               "G_pred_beta"=c(G_pred_beta),
+#               "n"=nrow(X), "m"=ncol(X), "max_iter"=max_iter, 
+#               "batch_size"=batch_size, "tol"=tol,
+#               "l2_dist_betas"=sum((pred_beta - beta)**2)**0.5)
+#   return (res)
+# }
 
-run_fixed_point(100)
+# run_fixed_point(100)
+# 
+# system.time(
+#   res_betas <- mclapply(c(1000, 3000, 5000, 10000, 15000), run_fixed_point, mc.cores = 5)
+# )
+# 
+# res_betas
 
-system.time(
-  res_betas <- mclapply(c(1000, 3000, 5000, 10000, 15000), run_fixed_point, mc.cores = 5)
-)
-
-res_betas
-
-save_plots <- function(res_betas, file_n='/Users/grigorkeropyan/Desktop/Stat/codes/plots/batch_64/') {
-  lgth <- length(res_betas)
-  for(i in 1:lgth) {
-    pred <- c(res_betas[[i]]$pred_beta)
-    actual <- res_betas[[i]]$gt_beta
-    title_name <- paste('sample size(n) ', res_betas[[i]]$n)
-    title_name <- paste(title_name, ", batch size ", sep='')
-    title_name <- paste(title_name, res_betas[[i]]$batch_size)
-    title_name <- paste(title_name, ", max iter ", sep='')
-    title_name <- paste(title_name, res_betas[[i]]$max_iter)
-    pl <- ggplot() + 
-      geom_point(aes(x=1:6, y=actual, colour="ground truth"), size=1.5) +
-      geom_point(aes(x=1:6, y=pred, colour="estimated"), size=1) + 
-      labs(title=title_name, 
-           x="beta1:6", y="ground truth and estimated beta's" ) +
-      scale_color_manual(name="legend", breaks=c("ground truth", "estimated"),
-                         values=c("ground truth"="black", "estimated"="red")) +
-      theme(legend.position = c(0.1,0.91), plot.title = element_text(hjust = 0.5))
-    
-    file_name <- paste(file_n, 'betas_')
-    file_name <- paste(file_name, res_betas[[i]]$n, sep='')
-    file_name <- paste(file_name, '_')
-    file_name <- paste(file_name, res_betas[[i]]$batch_size, sep='')
-    file_name <- paste(file_name, '.png', sep='')
-    ggsave(filename = file_name, plot = pl)
-    
-    pl_G <- ggplot() + 
-      geom_point(aes(x=1:6, y=res_betas[[i]]$G_beta, colour="G_beta"), size=1.5) +
-      geom_point(aes(x=1:6, y=res_betas[[i]]$G_pred_beta, colour="G_pred_beta"), size=1) + 
-      labs(title=title_name, 
-           x="beta1:6", y="ground truth and estimated G_beta's" ) +
-      scale_color_manual(name="legend", breaks=c("G_beta", "G_pred_beta"),
-                         values=c("G_beta"="black", "G_pred_beta"="red")) +
-      theme(legend.position = c(0.1,0.91), plot.title = element_text(hjust = 0.5))
-    
-    file_name_G <- paste(file_n, 'G_betas_')
-    file_name_G <- paste(file_name_G, res_betas[[i]]$n, sep='')
-    file_name_G <- paste(file_name_G, '_')
-    file_name_G <- paste(file_name_G, res_betas[[i]]$batch_size, sep='')
-    file_name_G <- paste(file_name_G, '.png', sep='')
-    ggsave(filename = file_name_G, plot = pl_G)
-  }
-}
-
-save_plots(res_betas)
+# save_plots <- function(res_betas, file_n='/Users/grigorkeropyan/Desktop/Stat/codes/plots/batch_64/') {
+#   lgth <- length(res_betas)
+#   for(i in 1:lgth) {
+#     pred <- c(res_betas[[i]]$pred_beta)
+#     actual <- res_betas[[i]]$gt_beta
+#     title_name <- paste('sample size(n) ', res_betas[[i]]$n)
+#     title_name <- paste(title_name, ", batch size ", sep='')
+#     title_name <- paste(title_name, res_betas[[i]]$batch_size)
+#     title_name <- paste(title_name, ", max iter ", sep='')
+#     title_name <- paste(title_name, res_betas[[i]]$max_iter)
+#     pl <- ggplot() + 
+#       geom_point(aes(x=1:6, y=actual, colour="ground truth"), size=1.5) +
+#       geom_point(aes(x=1:6, y=pred, colour="estimated"), size=1) + 
+#       labs(title=title_name, 
+#            x="beta1:6", y="ground truth and estimated beta's" ) +
+#       scale_color_manual(name="legend", breaks=c("ground truth", "estimated"),
+#                          values=c("ground truth"="black", "estimated"="red")) +
+#       theme(legend.position = c(0.1,0.91), plot.title = element_text(hjust = 0.5))
+#     
+#     file_name <- paste(file_n, 'betas_')
+#     file_name <- paste(file_name, res_betas[[i]]$n, sep='')
+#     file_name <- paste(file_name, '_')
+#     file_name <- paste(file_name, res_betas[[i]]$batch_size, sep='')
+#     file_name <- paste(file_name, '.png', sep='')
+#     ggsave(filename = file_name, plot = pl)
+#     
+#     pl_G <- ggplot() + 
+#       geom_point(aes(x=1:6, y=res_betas[[i]]$G_beta, colour="G_beta"), size=1.5) +
+#       geom_point(aes(x=1:6, y=res_betas[[i]]$G_pred_beta, colour="G_pred_beta"), size=1) + 
+#       labs(title=title_name, 
+#            x="beta1:6", y="ground truth and estimated G_beta's" ) +
+#       scale_color_manual(name="legend", breaks=c("G_beta", "G_pred_beta"),
+#                          values=c("G_beta"="black", "G_pred_beta"="red")) +
+#       theme(legend.position = c(0.1,0.91), plot.title = element_text(hjust = 0.5))
+#     
+#     file_name_G <- paste(file_n, 'G_betas_')
+#     file_name_G <- paste(file_name_G, res_betas[[i]]$n, sep='')
+#     file_name_G <- paste(file_name_G, '_')
+#     file_name_G <- paste(file_name_G, res_betas[[i]]$batch_size, sep='')
+#     file_name_G <- paste(file_name_G, '.png', sep='')
+#     ggsave(filename = file_name_G, plot = pl_G)
+#   }
+# }
+# 
+# save_plots(res_betas)
 
 # data <- simulate_rank_regression_data(1000, 6)
 # X <- data$X
@@ -405,7 +492,6 @@ save_plots(res_betas)
 # uniroot(func2, lower = 2, upper = 3)
 # newton_root_finding(func2, c(0, 0), grad_func2)
 
-# inverse_cdf_z<- inverse(cdf_z, -1000, 1000)
 # sub_vals <- c(2, 5, 6)
 # val <- cdf_z(10.95, sub_vals)
 # val
