1a5587c509933579d1e0d73a59a150c200952dc7
grigorkeropyan <goqorkeropyan@gmail.com>
2022-03-13 19:31:27 +0100

    added plot the bivariate results

---
+++ src/bivariate_pnl/bivariate_pnl_plot_res.R
@@ -0,0 +1,16 @@
+library(rjson)
+library(ggplot2)
+library(latex2exp)
+
+res <- fromJSON(file = "../res/bivariate_pnl/results_exp_l2.json")
+
+lambdas <- res$lambdas
+accuracy <- res$all_accs
+accuracy
+pl <- ggplot() + geom_point(aes(x=lambdas, y = accuracy), colour="red") +
+  labs(title = TeX(r'(expected rank ell2 algorithm, model $Y = (beta X + N)^{1/3}$)')) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+pl
+
+ggsave(filename = "../plots/bivariate_pnl/results_exp_l2_non_idf.png", plot = pl)
--- src/test.R
+++ src/test.R
@@ -1,18 +1,23 @@
-library(dHSIC)
+# library(dHSIC)
+# 
+# # non identifiability
+# sample_size <- 10000
+# beta <- runif(1, min = -100, max = 100)
+# x <- rnorm(sample_size)
+# noise_2 <- rnorm(sample_size)
+# y = sign(beta*x + noise_2)*abs((beta*x + noise_2))^(1/3)
+# 
+# dhsic(x, noise_2, kernel = "gaussian.fixed")$dHSIC
+# cor(x, noise_2)
+# 
+# noise_1 <- (x - beta*noise_2)/(beta^2+1)**0.5
+# dhsic(y^3, noise_1, kernel = "gaussian.fixed")$dHSIC
+# cor(y, noise_1)
+# 
+# plot(x, y)
+# plot(y, x)
 
-# non identifiability
-sample_size <- 10000
-beta <- runif(1, min = -100, max = 100)
-x <- rnorm(sample_size)
-noise_2 <- rnorm(sample_size)
-y = sign(beta*x + noise_2)*abs((beta*x + noise_2))^(1/3)
+args <- commandArgs(trailingOnly = T)
+print(args[1])
+print(args[2])
 
-dhsic(x, noise_2, kernel = "gaussian.fixed")$dHSIC
-cor(x, noise_2)
-
-noise_1 <- (x - beta*noise_2)/(beta^2+1)**0.5
-dhsic(y^3, noise_1, kernel = "gaussian.fixed")$dHSIC
-cor(y, noise_1)
-
-plot(x, y)
-plot(y, x)
