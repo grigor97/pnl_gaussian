f821424dce2bc0448b367ae3e70cf1ef73af02bc
grigorkeropyan <goqorkeropyan@gmail.com>
2022-03-14 10:26:02 +0100

    chnaged plot

--- src/bivariate_pnl/bivariate_pnl_plot_res.R
+++ src/bivariate_pnl/bivariate_pnl_plot_res.R
@@ -2,15 +2,29 @@ library(rjson)
 library(ggplot2)
 library(latex2exp)
 
-res <- fromJSON(file = "../res/bivariate_pnl/results_idf_expected_l2_rank_100_1000.json")
+res_l2 <- fromJSON(file = "../res/bivariate_pnl/results_exp_l2.json")
+res_l1 <- fromJSON(file = "../res/bivariate_pnl/results_exp_l1.json") 
 
-lambdas <- res$lambdas
-accuracy <- res$all_accs
-accuracy
-pl <- ggplot() + geom_point(aes(x=lambdas, y = accuracy), colour="red") +
-  labs(title = TeX(r'(expected rank ell2 algorithm, model $Y = (beta X^2 + N)^{1/3}$)')) +
-  theme(plot.title = element_text(hjust = 0.5))
+res_fixed <- fromJSON(file = "../res/bivariate_pnl/results_fixed_point.json")
+res_fixed
+
+lambdas_l2 <- res_l2$lambdas
+accuracy_l2 <- res_l2$all_accs
+accuracy_l2
+
+lambdas_l1 <- res_l1$lambdas
+accuracy_l1 <- res_l1$all_accs
+accuracy_l1
+
+pl <- ggplot() + geom_line(aes(x=lambdas_l2, y = accuracy_l2, colour="ell2")) +
+  geom_line(aes(x=lambdas_l1, y = accuracy_l1, colour="ell1")) +
+  geom_line(aes(x=lambdas_l1, y = rep(res_fixed$all_accs[2], length(lambdas_l1)), colour="fixed point")) +
+  labs(title = TeX(r'(model $X_2 = (beta X_1 + N)^{1/3}$)'), x=TeX(r'(\lambda)'), y="accuracy") +
+  scale_color_manual(name='',
+                     breaks=c('ell2', 'ell1', "fixed point"),
+                     values=c('blue', 'red', "black")) +
+  theme(legend.position='top', plot.title = element_text(hjust = 0.5))
 
 pl
 
-ggsave(filename = "../plots/bivariate_pnl/results_exp_l2_idf.png", plot = pl)
+ggsave(filename = "../plots/bivariate_pnl/results_exp_non_idf.png", plot = pl)
