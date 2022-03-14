b66b025bd2efa04999267a92b124fe3f55cb6d67
grigorkeropyan <goqorkeropyan@gmail.com>
2022-03-14 09:38:08 +0100

    chnaged run bivariate full hsic

--- src/bivariate_pnl/bivariate_pnl_plot_res.R
+++ src/bivariate_pnl/bivariate_pnl_plot_res.R
@@ -2,15 +2,15 @@ library(rjson)
 library(ggplot2)
 library(latex2exp)
 
-res <- fromJSON(file = "../res/bivariate_pnl/results_exp_l2.json")
+res <- fromJSON(file = "../res/bivariate_pnl/results_idf_expected_l2_rank_100_1000.json")
 
 lambdas <- res$lambdas
 accuracy <- res$all_accs
 accuracy
 pl <- ggplot() + geom_point(aes(x=lambdas, y = accuracy), colour="red") +
-  labs(title = TeX(r'(expected rank ell2 algorithm, model $Y = (beta X + N)^{1/3}$)')) +
+  labs(title = TeX(r'(expected rank ell2 algorithm, model $Y = (beta X^2 + N)^{1/3}$)')) +
   theme(plot.title = element_text(hjust = 0.5))
 
 pl
 
-ggsave(filename = "../plots/bivariate_pnl/results_exp_l2_non_idf.png", plot = pl)
+ggsave(filename = "../plots/bivariate_pnl/results_exp_l2_idf.png", plot = pl)
--- src/run_full_hsic_bivariate_pnl.R
+++ src/run_full_hsic_bivariate_pnl.R
@@ -1,9 +1,9 @@
 source("multivariate_pnl/full_hsic_pnl.R")
-source("bivariate_pnl/rank_reg_bivariate_pnl.R")
+source("utils.R")
+library(parallel)
 
-correct_num <- 0
-for(i in 1:100) {
-  data <- simulate_bivariate_pnl(n=100)
+run_bivariate_hsic <- function(sample_size) {
+  data <- simulate_bivariate_pnl_idf(n=sample_size)
   
   df <- data.frame(data$data)
   gt_beta <- data$beta
@@ -13,9 +13,53 @@ for(i in 1:100) {
   
   res <- order_recovery_by_last_node(df)
   print(res)
-  if(res[1] == "X1") {
-    correct_num <- correct_num + 1
-  }
+  return(res[1] == "X1")
 }
 
-correct_num
+# run_bivariate_hsic(100)
+
+alg <- "full_hsic" 
+sample_sizes <- c(500, 1000, 3000)
+
+all_accs <- c()
+
+
+
+num_datasets <- 100
+
+numCores <- detectCores() - 1
+print(paste("num coress  --- ", numCores))
+
+for(n in sample_sizes) {
+  dummy_fun <- function(i) {
+    return(run_bivariate_hsic(sample_size=n))
+  }
+  
+  system.time(
+    results <- mclapply(1:num_datasets, dummy_fun, mc.cores = numCores)
+  )
+  
+  results <- unlist(results)
+  acc <- sum(results)/length(results)
+  print(paste("for alg ", alg))
+  print(paste("and sample size ", n))
+  print(paste("finished, accuracy is ", acc))
+  all_accs <- c(all_accs, acc)
+}
+
+res <- list("sample_sizes"=sample_sizes, "all_accs"=all_accs, "alg"=alg)
+
+res
+
+save_file <- "../res/bivariate_pnl/results_idf"
+save_file <- paste(save_file, alg, sep="_")
+save_file <- paste(save_file, num_datasets, sep="_")
+save_file <- paste(save_file, ".json", sep="")
+
+json_data <- toJSON(res)
+write(json_data, save_file)
+
+
+# ress <- fromJSON(file = "../res/bivariate_pnl/results.json")
+# ress
+
