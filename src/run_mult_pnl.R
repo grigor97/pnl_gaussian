2653c0cba3b83cc6ddd4a9c37201520c35776c84
grigorkeropyan <goqorkeropyan@gmail.com>
2022-03-16 13:39:36 +0100

    added code part to run parralel for number of datasets

--- src/run_mult_pnl.R
+++ src/run_mult_pnl.R
@@ -27,9 +27,43 @@ run_mult_rank_reg <- function(sample_size, rank_alg, lamb) {
 }
 
 # res_hsic <- run_mult_full_hsic(100)
-res_rank <- run_mult_rank_reg(500, rank_alg="expected_l1_rank", lamb=10)
+# res_rank <- run_mult_rank_reg(500, rank_alg="expected_l1_rank", lamb=10)
 # res_rank <- run_mult_rank_reg(500, rank_alg="expected_l2_rank", lamb=10)
 # res_rank <- run_mult_rank_reg(500, rank_alg="fixed_point", lamb=10)
+
+num_datasets <- 100
+sample_size <- 1000
+alg <- "expected_l1_rank"
+
+numCores <- detectCores() - 1
+print(paste("num coress  --- ", numCores))
+
+
+dummy_fun <- function(i) {
+  if(alg == "full_hsic") {
+    return(run_mult_full_hsic(sample_size))
+  } else {
+    return(run_mult_rank_reg(sample_size, rank_alg=alg, lamb=10))
+  }
+}
+
+system.time(
+  results <- mclapply(1:num_datasets, dummy_fun, mc.cores = numCores)
+)
+results <- unlist(results)
+pred_order <- matrix(unlist(results), ncol = 4, nrow=length(results)/4, byrow = T)
+
+res <- list("alg"=alg, "num_datasets"=num_datasets, "sample_size"=sample_size,
+            "pred_order"=pred_order)
+
+save_file <- "../res/mult_pnl/results4_"
+save_file <- paste(save_file, alg, sep="_")
+save_file <- paste(save_file, num_datasets, sep="_")
+save_file <- paste(save_file, ".json", sep="")
+
+json_data <- toJSON(res)
+write(json_data, save_file)
+
 
 
 
