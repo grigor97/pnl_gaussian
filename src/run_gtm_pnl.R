aaefe33502ba74a7a213f702a1a69c4afdb3f647
grigorkeropyan <goqorkeropyan@gmail.com>
2022-07-13 09:46:17 +0200

    error handling in foreach

--- src/run_gtm_pnl.R
+++ src/run_gtm_pnl.R
@@ -6,13 +6,17 @@ no_cores <- detectCores()
 cl <- makeCluster(no_cores-1)
 registerDoParallel(cl) 
 
-n = 100
-num_datasets <- 4
+n = 10
+num_datasets <- 2
 # .errorhandling="remove"
 res <- foreach(i=1:num_datasets, .combine="rbind", .packages = c("dHSIC")) %dopar% {
+  error=S.j.hat.ux(x) {return(NA)}
   X <- simulate.bv.pnl.gtm(n)
   df <- data.frame(X)
   ord <- order_recovery_gtm(df)
+  print("-----------------")
+  print(ord)
+  print("-----------------")
   if(strtoi(substr(ord[1], 2, 100))==1) {
     return(T)
   } 
